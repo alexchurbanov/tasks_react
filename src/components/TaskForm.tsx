96549fb6dd41edea1c8162c532121c108c6dba7a
Alexander Churbanov <alexanderxchurbanov@gmail.com>
2022-01-09 22:22:09 +0200

    update style for form

--- src/components/TaskForm.tsx
+++ src/components/TaskForm.tsx
@@ -4,7 +4,7 @@ function TaskForm() {
 	return (
 		<form className='task-form'>
 			<input className='form-input'/>
-			<button className='form-button' type={"button"}>Add Task</button>
+			<button className='form-button' type='button'>+</button>
 		</form>
 	)
 }
