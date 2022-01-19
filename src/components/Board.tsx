c8cfdb1213173160ae18dc17616c5ef4e30aea63
Alexander Churbanov <alexanderxchurbanov@gmail.com>
2022-01-19 17:46:21 +0200

    add react dnd remove webVitals

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,14 +3,19 @@ import TaskBox from './TaskBox';
 import AddTaskBoxButton from './AddTaskBoxButton';
 import {useAppSelector} from '../store/hooks';
 import {selectTaskBoxes} from '../store/slices/TaskBoxSlice';
+import {useDrop} from "react-dnd";
 
 function Board() {
+	const [,drop] = useDrop({
+		accept: 'taskBox',
+		drop: (dragItem) => {console.log(dragItem)},
+	})
 	const list = useAppSelector(selectTaskBoxes).map(item => {
 		return <TaskBox key={item.id} item={item}/>;
 	});
 
 	return (
-		<div className='board'>
+		<div className='board' ref={drop}>
 			{list}
 			<AddTaskBoxButton/>
 		</div>
--- src/index.tsx
+++ src/index.tsx
@@ -3,20 +3,18 @@ import ReactDOM from 'react-dom';
 import 'normalize.css';
 import './styles/index.sass';
 import Board from './components/Board';
-import reportWebVitals from './reportWebVitals';
 import {store} from './store/store';
 import {Provider} from 'react-redux';
+import {DndProvider} from "react-dnd";
+import {HTML5Backend} from "react-dnd-html5-backend";
 
 ReactDOM.render(
 	<React.StrictMode>
-		<Provider store={store}>
-			<Board/>
-		</Provider>
+		<DndProvider backend={HTML5Backend}>
+			<Provider store={store}>
+				<Board/>
+			</Provider>
+		</DndProvider>
 	</React.StrictMode>,
 	document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log);
