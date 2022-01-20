9d365877efd4f2ee98bd31f10fbafa07a597a06d
Alexander Churbanov <alexanderxchurbanov@gmail.com>
2022-01-20 17:21:14 +0200

    add drag n drop to TaskBoxes

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -3,20 +3,20 @@ import TaskBox from './TaskBox';
 import AddTaskBoxButton from './AddTaskBoxButton';
 import {useAppSelector} from '../store/hooks';
 import {selectTaskBoxes} from '../store/slices/TaskBoxSlice';
-import {useDrop} from "react-dnd";
+import {useEffect, useState} from "react";
 
 function Board() {
-	const [,drop] = useDrop({
-		accept: 'taskBox',
-		drop: (dragItem) => {console.log(dragItem)},
-	})
-	const list = useAppSelector(selectTaskBoxes).map(item => {
-		return <TaskBox key={item.id} item={item}/>;
-	});
+	const selected = useAppSelector(selectTaskBoxes);
+	const [taskBoxes, setTaskBoxes] = useState(selected);
+	useEffect(() => {
+		setTaskBoxes(selected);
+	}, [selected])
 
 	return (
-		<div className='board' ref={drop}>
-			{list}
+		<div className='board'>
+			{taskBoxes.map((item, index) => {
+				return <TaskBox key={item.id} item={item} index={index}/>;
+			})}
 			<AddTaskBoxButton/>
 		</div>
 	)
