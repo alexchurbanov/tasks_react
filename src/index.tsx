2926a67d7ef59264e05c4d6ca3b7d30496babf8f
Alexander Churbanov <alexanderxchurbanov@gmail.com>
2022-01-26 21:06:34 +0200

    allow touch drag and drop

--- src/index.tsx
+++ src/index.tsx
@@ -7,11 +7,12 @@ import {store} from './store/store';
 import {Provider} from 'react-redux';
 import {DndProvider} from "react-dnd";
 import {HTML5Backend} from "react-dnd-html5-backend";
+import {TouchBackend} from "react-dnd-touch-backend";
 
 ReactDOM.render(
 	<React.StrictMode>
 		<Provider store={store}>
-			<DndProvider backend={HTML5Backend}>
+			<DndProvider backend={window.matchMedia("only screen and (max-width: 760px)").matches ? TouchBackend : HTML5Backend}>
 				<Board/>
 			</DndProvider>
 		</Provider>
